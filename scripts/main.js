b9d782c81af8adbc488ea5fb01cf64819399fbd1
Kyn Chong <kyn.chong@gmail.com>
2022-03-05 11:24:35 -0800

    Prototype 3.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
-/* SWIPER CONFIGURATION */
+/* SWIPER IMAGE CAROUSEL CONFIGURATION */
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
+    preloadImages: true,
+    updateOnImagesReady: true,
 
     // If we need pagination
     pagination: {
@@ -24,7 +26,8 @@ const swiper = new Swiper('.swiper', {
     */
 });
 
-/* EVENT DELEGATION HANDLER */
+
+/* Elements */
 const body = document.querySelector("body");
 const main = document.querySelector("main");
 const imageModal = document.querySelector(".carousel-modal");
@@ -32,7 +35,7 @@ const closeCarouselButton = document.querySelector(".carousel-modal-close");
 const swiperWrapper = document.querySelector(".swiper-wrapper");
 const selectedGalleryImage = "gallery-item-anchor";
 
-// Swiper element
+/* Returns a "slide" to append to Swiper carousel */
 const getSwiperElement = (url, caption) => {
     let swiperDiv = document.createElement("div");
     let swiperDivImg = document.createElement("img");
@@ -45,35 +48,36 @@ const getSwiperElement = (url, caption) => {
     return swiperDiv;
 }
 
-// Toggles display of carousel modal and associated images
-const toggleModal = (touchEvent) => {
-    touchEvent.preventDefault();
-    // Acquire links for carousel images
-    if (touchEvent.target.className == selectedGalleryImage) {
-        let target = touchEvent.target;
+/* Show/Hide image carousel modal */
+const toggleModal = (interaction) => {
+
+    // Determine if the element clicked was a gallery item
+    if (interaction.target.className == selectedGalleryImage) {
+        let target = interaction.target;
         let children = target.children;
 
-        // reset swiper
-        swiper.removeAllSlides();
-
-        // Send image urls to get a swiper element to be appended to carousel
+        // Add images to carousel
         for (child of children) {
             let src = child.getAttribute("data-src");
             let caption = child.getAttribute("alt");
             swiperWrapper.append(getSwiperElement(src, caption));
         }
 
+        // Update Swiper of images added
+        swiper.update();
+
         // shows modal
         imageModal.style.display = "flex";
         body.classList.toggle("noscroll");
 
-    } else if (touchEvent.target == imageModal || touchEvent.target == closeCarouselButton) {
-        // hides modal
+    } else if (interaction.target == imageModal || interaction.target == closeCarouselButton) {
+        // hide modal
         imageModal.style.display = "none";
-
-        // resets modal html
-        swiperWrapper.innerHTML = "";
         body.classList.toggle("noscroll");
+
+        // Update Swiper of images removed
+        swiper.removeAllSlides();
+        swiper.update();
     }
 }
 
